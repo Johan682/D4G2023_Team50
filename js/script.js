dd4b314896dc87b8438c07fe173e2dd2f11986db
Johan Loya <63602648+Johan682@users.noreply.github.com>
2023-11-15 14:40:07 +0000

    c

--- js/script.js
+++ js/script.js
@@ -4,10 +4,9 @@ function restaurerDonnees() {
 
 function genererPDF() {
 var url = document.getElementById('urlInput').value;
-
+console.log(url)
 }
 
 function print_JSON(){
-   
-
+   let url= '/referentiel-general-ecoconception-version-v1.json';
 }
