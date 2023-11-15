a6806b97a333492cf4314c48a2c35bb5533d4ba6
Johan Loya <63602648+Johan682@users.noreply.github.com>
2023-11-15 23:01:44 +0000

    c

--- js/script.js
+++ js/script.js
@@ -7,94 +7,39 @@ var url = document.getElementById('urlInput').value;
 console.log(url)
 }
 
-function chargementpage() {
-    fetch("referentiel-general-ecoconception-version-v1.json")
-        .then(response => response.json())
-        .then(data => {
-            // Get the table body to populate data
-            const tableBody = document.querySelector("#dataTable tbody");
+function addRowToTable(field, value, rowIndex) {
+    const row = tableBody.insertRow();
+    const cell1 = row.insertCell(0);
+    const cell2 = row.insertCell(1);
+    const cell3 = row.insertCell(2); // Ajout de la troisième colonne
 
-            // Function to add a row to the table
-            function addRowToTable(field, value) {
-                const row = tableBody.insertRow();
-                const cell1 = row.insertCell(0);
-                const cell2 = row.insertCell(1);
-                const cell3 = row.insertCell(2); // Ajout de la troisième colonne
+    cell1.textContent = field;
+    cell2.innerHTML = value;
 
-                cell1.textContent = field;
-                cell2.innerHTML = value;
+    // Création des choix
+    const choices = ["Conforme", "En cours de déploiement", "Non conforme", "Non applicable"];
 
-                // Création des choix
-                const choices = ["Conforme", "En cours de déploiement", "Non conforme", "Non applicable"];
+    // Ajout des choix en tant que boutons radio
+    choices.forEach((choiceText, index) => {
+        const radioBtn = document.createElement("input");
+        radioBtn.type = "radio";
+        radioBtn.name = `etat_${field.replace(/ /g, "_")}_${rowIndex}`;
+        radioBtn.value = choiceText.toLowerCase().replace(/ /g, "_");
+        radioBtn.id = `etat_${field.replace(/ /g, "_")}_${choiceText.toLowerCase().replace(/ /g, "_")}_${rowIndex}`;
+        
+        // Ajout du bouton radio à la cellule
+        cell3.appendChild(radioBtn);
 
-                // Ajout des choix en tant que boutons radio
-                choices.forEach(choiceText => {
-                    const radioBtn = document.createElement("input");
-                    radioBtn.type = "radio";
-                    radioBtn.name = `etat_${field.replace(/ /g, "_")}`;
-                    radioBtn.value = choiceText.toLowerCase().replace(/ /g, "_");
-                    radioBtn.id = `etat_${field.replace(/ /g, "_")}_${choiceText.toLowerCase().replace(/ /g, "_")}`;
-                    
-                    // Ajout du bouton radio à la cellule
-                    cell3.appendChild(radioBtn);
+        // Création de l'étiquette pour le bouton radio
+        const label = document.createElement("label");
+        label.htmlFor = `etat_${field.replace(/ /g, "_")}_${choiceText.toLowerCase().replace(/ /g, "_")}_${rowIndex}`;
+        label.textContent = choiceText;
 
-                    // Création de l'étiquette pour le bouton radio
-                    const label = document.createElement("label");
-                    label.htmlFor = `etat_${field.replace(/ /g, "_")}_${choiceText.toLowerCase().replace(/ /g, "_")}`;
-                    label.textContent = choiceText;
-
-                    // Ajout de l'étiquette à la cellule
-                    cell3.appendChild(label);
-                });
-            }
-
-            data.criteres.forEach(critere => {
-                addRowToTable(critere.thematique, critere.critere);
-            });
-        })
-        .catch(error => {
-            console.error("Error fetching data:", error);
-        }); 
+        // Ajout de l'étiquette à la cellule
+        cell3.appendChild(label);
+    });
 }
 
-function calculerScore() {
-    const tableBody = document.querySelector("#dataTable tbody");
-    let critereConforme = 0;
-    let critereNonApplicable = 0;
-
-    // Parcours des lignes du tableau
-    for (let i = 0; i < tableBody.rows.length; i++) {
-        const row = tableBody.rows[i];
-        
-        // Obtention du bouton radio sélectionné de la troisième colonne
-        const radioBtn = row.cells[2].querySelector("input[type=radio]:checked");
-        
-        // Vérification du bouton radio sélectionné
-        if (radioBtn) {
-            const selectedValue = radioBtn.value;
-        
-            // Si le critère est conforme
-            if (selectedValue === "conforme") {
-                critereConforme++;
-            }
-        
-            // Si le critère est non applicable
-            if (selectedValue === "non_applicable") {
-                critereNonApplicable++;
-            }
-        }
-    }
-
-    // Total des critères (79)
-    const totalCritere = 79;
-
-    // Calcul du score de conformité
-    const score = critereConforme / (totalCritere - critereNonApplicable);
-    
-    // Affichage du score dans l'élément HTML
-    const scoreContainer = document.getElementById("scoreContainer");
-
-    // Mise à jour du contenu de l'élément avec le score
-    scoreContainer.innerHTML = score.toFixed(2); // pour afficher le score avec deux décimales
-    // Affichage du score dans la console (à adapter selon tes besoins)
-}
+data.criteres.forEach((critere, index) => {
+    addRowToTable(critere.thematique, critere.critere, index);
+});
