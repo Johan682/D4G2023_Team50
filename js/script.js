daf81677b98978197b139acd2c351e8275771675
Johan Loya <63602648+Johan682@users.noreply.github.com>
2023-11-15 15:04:45 +0000

    c

--- js/script.js
+++ js/script.js
@@ -8,5 +8,38 @@ console.log(url)
 }
 
 function print_JSON(){
-   let url= '/referentiel-general-ecoconception-version-v1.json';
-}
+   
+   
+        // Fonction pour charger et afficher les données JSON
+        function chargerEtAfficherJSON() {
+          // Créer une instance de XMLHttpRequest
+          var xhr = new XMLHttpRequest();
+    
+          // Spécifier le chemin vers le fichier JSON local
+          var cheminFichierJSON = '/referentiel-general-ecoconception-version-v1.json';
+    
+          // Configurer la requête
+          xhr.open('GET', cheminFichierJSON, true);
+    
+          // Gérer l'événement de chargement
+          xhr.onload = function() {
+            if (xhr.status === 200) {
+              // Parse le contenu JSON
+              var donneesJSON = JSON.parse(xhr.responseText);
+    
+              // Afficher les données dans la page
+              document.getElementById('affichage').textContent = JSON.stringify(donneesJSON, null, 2);
+            } else {
+              console.error('Erreur lors du chargement du fichier JSON. Statut : ' + xhr.status);
+            }
+          };
+    
+          // Gérer les erreurs réseau
+          xhr.onerror = function() {
+            console.error('Erreur réseau lors du chargement du fichier JSON.');
+          };
+    
+          // Envoyer la requête
+          xhr.send();
+        }
+    }
