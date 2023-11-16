3f9af49a7cf293775aa9fede07c8d4c29d13e24c
Johan Loya <63602648+Johan682@users.noreply.github.com>
2023-11-16 02:36:56 +0000

    c

--- js/script.js
+++ js/script.js
@@ -37,7 +37,7 @@ function chargementpage() {
                 const form = document.createElement("form");
                 form.id = `radioGroup_${field}`;
 
-                const options = ["Conforme", "Encoursdedeploiement", "Nonconforme", "Nonapplicable"];
+                const options = ["Conforme", "test", "Nonconforme", "Nonapplicable"];
 
                 options.forEach(optionText => {
                     const label = document.createElement("label");
