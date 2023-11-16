f4a8341f6efc5f71ee2b999dce1b5094a3e13a49
Johan Loya <63602648+Johan682@users.noreply.github.com>
2023-11-16 13:56:26 +0000

    c

--- js/script.js
+++ js/script.js
@@ -1,35 +1,28 @@
-
-function chargementpage() {
+function chargementPage() {
     fetch("referentiel-general-ecoconception-version-v1.json")
         .then(response => response.json())
         .then(data => {
-            // Get the table body to populate data
             const tableBody = document.querySelector("#dataTable tbody");
 
-            // Function to add a row to the table
             function addRowToTable(field, value) {
                 const row = tableBody.insertRow();
                 const cell1 = row.insertCell(0);
                 const cell2 = row.insertCell(1);
-                const cell3 = row.insertCell(2); // Ajout de la troisième colonne
+                const cell3 = row.insertCell(2);
 
                 cell1.textContent = field;
                 cell2.innerHTML = value;
 
-                // Création du fieldset
                 const fieldset = document.createElement("fieldset");
                 fieldset.id = `fieldset_${field}`;
 
                 const legend = document.createElement("legend");
-                //legend.textContent = "Statut du critère";
-
                 fieldset.appendChild(legend);
 
-                // Création du formulaire avec des boutons radio
                 const form = document.createElement("form");
                 form.id = `radioGroup_${field}`;
 
-                const options = ["conforme", "en cours de deploiement", "non conforme", "nonapplicable"];
+                const options = ["conforme", "en cours de déploiement", "non conforme", "non applicable"];
 
                 options.forEach(optionText => {
                     const label = document.createElement("label");
@@ -45,7 +38,6 @@ function chargementpage() {
                     form.appendChild(label);
                 });
 
-                // Ajout du formulaire à la cellule
                 fieldset.appendChild(form);
                 cell3.appendChild(fieldset);
             }
@@ -67,87 +59,93 @@ function calculerScore() {
     let critereConforme = 0;
     let critereNonApplicable = 0;
 
-    // Parcours des lignes du tableau
     for (let i = 0; i < tableBody.rows.length; i++) {
         const row = tableBody.rows[i];
         const radioInputs = row.cells[2].querySelectorAll("input[type=radio]:checked");
 
-        // Si un bouton radio est sélectionné
         if (radioInputs.length > 0) {
             const selectedValue = radioInputs[0].value;
 
-            // Si le critère est conforme
             if (selectedValue === "conforme") {
                 critereConforme++;
             }
 
-            // Si le critère est non applicable
             if (selectedValue === "nonapplicable") {
                 critereNonApplicable++;
             }
         }
     }
-    
 
-    // Total des critères (79)
     const totalCritere = 79;
 
-    // Calcul du score de conformité
     const score = critereConforme / (totalCritere - critereNonApplicable);
 
-    // Affichage du score dans l'élément HTML
     const scoreContainer = document.getElementById("scoreContainer");
 
-    scoreContainer.innerHTML = score.toFixed(2); // pour afficher le score avec deux décimales
-    
-    // Mise à jour du contenu de l'élément avec le score
+    scoreContainer.innerHTML = score.toFixed(2);
+
     if (dropdownList.style.display === "none" || dropdownList.style.display === "") {
         dropdownList.style.display = "block";
     } else {
         dropdownList.style.display = "none";
     }
-    
 }
+
 function filtrerCriteres() {
     const tableBody = document.querySelector("#dataTable tbody");
-    const themeFilter = document.getElementById("themeFilter").value.toLowerCase(); // Converti en minuscules
-    const etatFilter = document.getElementById("etatFilter").value; // Converti en minuscules
+    const themeFilter = document.getElementById("themeFilter").value.toLowerCase();
+    const etatFilter = document.getElementById("etatFilter").value;
 
-    // Parcours des lignes du tableau
     for (let i = 0; i < tableBody.rows.length; i++) {
         const row = tableBody.rows[i];
-        const themeValue = row.cells[0].textContent.toLowerCase().replace(/ /g, "_"); // Converti en minuscules
+        const themeValue = row.cells[0].textContent.toLowerCase().replace(/ /g, "_");
         const etatInputs = row.cells[2].querySelectorAll("input[type=radio]:checked");
         const etatValue = etatInputs.length > 0 ? etatInputs[0].value : "";
 
-        // Affiche ou masque la ligne en fonction des filtres sélectionnés
         const afficherLigne =
             (themeFilter === "toutes" || themeFilter === themeValue) &&
             (etatFilter === "tous" || etatFilter === etatValue);
 
-        // Met à jour la visibilité de la ligne
         row.style.display = afficherLigne ? "" : "none";
     }
-    
 }
 
 function trierTableauParTheme() {
     const tableBody = document.querySelector("#dataTable tbody");
 
-    // Convertir les lignes du tableau en un tableau
     const rowsArray = Array.from(tableBody.rows);
 
-    // Trier le tableau en fonction du thème (première colonne)
     rowsArray.sort((a, b) => {
         const themeA = a.cells[0].textContent.toLowerCase();
         const themeB = b.cells[0].textContent.toLowerCase();
         return themeA.localeCompare(themeB);
     });
 
-    // Supprimer toutes les lignes du tableau actuel
     tableBody.innerHTML = "";
 
-    // Ajouter les lignes triées au tableau
+    rowsArray.forEach(row => {
+        tableBody.appendChild(row);
+    });
+}
+
+function trierCriteres(etat) {
+    const tableBody = document.querySelector("#dataTable tbody");
+
+    const rowsArray = Array.from(tableBody.rows);
+
+    rowsArray.sort((a, b) => {
+        const etatA = a.cells[2].querySelector(`input[type=radio][value=${etat}]`);
+        const etatB = b.cells[2].querySelector(`input[type=radio][value=${etat}]`);
+
+        if (!etatA && !etatB) return 0;
+        if (!etatA) return 1;
+        if (!etatB) return -1;
+
+        return etatA.compareDocumentPosition(etatB) === Node.DOCUMENT_POSITION_FOLLOWING ? -1 : 1;
+    });
+
+    tableBody.innerHTML = "";
+
     rowsArray.forEach(row => {
         tableBody.appendChild(row);
     });
@@ -155,10 +153,10 @@ function trierTableauParTheme() {
 
 let reponsesIntermediaires = [];
 let reponseFinale = {};
+
 function enregistrer() {
     const tableBody = document.querySelector("#dataTable tbody");
 
-    // Exemple : stocker les réponses intermédiaires dans un tableau
     const reponses = [];
 
     for (let i = 0; i < tableBody.rows.length; i++) {
@@ -168,7 +166,6 @@ function enregistrer() {
         const radioInputs = row.cells[2].querySelectorAll("input[type=radio]:checked");
         const etat = radioInputs.length > 0 ? radioInputs[0].value : "";
 
-        // Stocker les réponses intermédiaires dans un objet
         reponses.push({
             theme: theme,
             value: value,
@@ -176,32 +173,24 @@ function enregistrer() {
         });
     }
 
-    // Ajouter les réponses intermédiaires à la liste globale
     reponsesIntermediaires.push(reponses);
     console.log();
 
-    // Réinitialiser le tableau pour de nouvelles réponses
     resetTable(tableBody);
 }
 
 function resetTable(tableBody) {
-    // Réinitialiser le contenu du tableau
     tableBody.innerHTML = "";
-
-    // Exemple : ajouter une ligne vide pour chaque critère
 
     data.criteres.forEach(critere => {
         addRowToTable(critere.thematique, critere.critere, "");
         console.log();
     });
-
 }
 
 function exportToPdf() {
-    // Sélectionnez l'élément à convertir en PDF
     const element = document.body;
 
-    // Options pour la conversion en PDF
     const options = {
         margin: 5,
         filename: 'rapport_audit.pdf',
@@ -210,60 +199,5 @@ function exportToPdf() {
         jsPDF: { unit: 'mm', format: 'a4', orientation: 'portrait' }
     };
 
-    // Utilisez html2pdf pour générer le PDF
     html2pdf(element, options);
 }
-
-function trierCriteres() {
-    const tableBody = document.querySelector("#dataTable tbody");
-    const themeSort = document.getElementById("Filtre_Thema").value; // Converti en minuscules
-    const etatSort = document.getElementById("etatFilter").value; // Converti en minuscules
-
-    // Convertit les lignes du tableau en un tableau ordinaire
-    const rowsArray = Array.from(tableBody.rows);
-
-    // Fonction de comparaison pour le tri
-    const compareRows = (row1, row2) => {
-        const themeValue1 = row1.cells[0].textContent; // Converti en minuscules
-        const themeValue2 = row2.cells[0].textContent.toLowerCase(); // Converti en minuscules
-
-        const etatInputs1 = row1.cells[2].querySelectorAll("input[type=radio]:checked");
-        const etatValue1 = etatInputs1.length > 0 ? etatInputs1[0].value : "";
-
-        const etatInputs2 = row2.cells[2].querySelectorAll("input[type=radio]:checked");
-        const etatValue2 = etatInputs2.length > 0 ? etatInputs2[0].value : "";
-
-        // Comparaison en fonction des critères de tri
-        if (themeSort !== "toutes") {
-            if (themeValue1 !== themeSort && themeValue2 !== themeSort) {
-                return 0; // Les deux ne correspondent pas au thème de tri, pas de changement d'ordre
-            } else if (themeValue1 !== themeSort) {
-                return 1; // row1 doit aller après row2 car il ne correspond pas au thème de tri
-            } else if (themeValue2 !== themeSort) {
-                return -1; // row1 doit aller avant row2 car il ne correspond pas au thème de tri
-            }
-        }
-
-        if (etatSort !== "tous") {
-            if (etatValue1 !== etatSort && etatValue2 !== etatSort) {
-                return 0; // Les deux ne correspondent pas à l'état de tri, pas de changement d'ordre
-            } else if (etatValue1 !== etatSort) {
-                return 1; // row1 doit aller après row2 car il ne correspond pas à l'état de tri
-            } else if (etatValue2 !== etatSort) {
-                return -1; // row1 doit aller avant row2 car il ne correspond pas à l'état de tri
-            }
-        }
-
-        // Aucun critère de tri spécifique, pas de changement d'ordre
-        return 0;
-    };
-
-    // Trie le tableau ordinaire en fonction de la fonction de comparaison
-    rowsArray.sort(compareRows);
-
-    // Vide le contenu du tableau et ajoute les lignes triées
-    tableBody.innerHTML = "";
-    rowsArray.forEach(row => tableBody.appendChild(row));
-}
-
-
